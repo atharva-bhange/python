7fc72505d64f091f3c392e76582c02d1d951f5e6
atharva-bhange <atharva.bhange@gmail.com>
2020-01-16 23:48:37 +0530

    num to string

---
+++ numbertoword.py
@@ -0,0 +1,12 @@
+'''
+# Number In Words
+Take any number as input and print its digits in words as output
+E.g. input - 136
+        Output - one three six
+'''
+str = ""
+convert = {'1' : "one",'2' : "two", '3' : "three",'4' : "four", '5':"five" , '6' : "six" , '7' : "seven" , '8' : "eight" , '9': "nine"}
+num = list(input("type the number:- "))
+for t in num:
+    str = str+" "+convert[t]
+print(str)
