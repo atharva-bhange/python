78d4d243f94c4916b0388983cf8e9f9c557bc4eb
atharva-bhange <atharva.bhange@gmail.com>
2020-01-29 19:47:15 +0530

    new

--- insertsort.py
+++ insertsort.py
@@ -9,7 +9,7 @@ print("-------------------------------------------------------")
 print("\n")
 
 import random
-#import timer
+
 nums = []
 
 while True:
@@ -20,59 +20,12 @@ while True:
         break
     elif num.startswith('random'):
         for t in range(int(num[6:])):
-            nums.append(random.randint(0,10000000000000000))
+            nums.append(random.randint(0,20))
         break
     else:
         print("Type a number or 'done' to view results.")
-originalnums = nums
-
-# BUBBLE SORT
-
-def swap(i1 ,i2):
-    nums.insert(i1 , nums[i2])
-    nums.pop(i2+1)
-#print(nums)
-check = 0
-loopcounterout =0
-loopcounterin = 0
-while loopcounterout<len(nums)-1:
-    loopcounterout += 1
-    for t in range(len(nums)-1):
-        loopcounterin += 1
-        if nums[t+1] < nums[t]:
-            swap(t , t+1)
-            check += 1
-    if check  ==0  :
-        break
-    check =0
-#print(nums)
-print(loopcounterout+loopcounterin, 'loops were required for sorting using bubble sort.')
-
-nums = originalnums
-
-# selection SORT
-
-loopcounter =0
-min = None # storing index not actual value
-
-for shifter in range(0,len(nums)):
-    loopcounter += 1
-    for checker in range(shifter , len(nums)):
-        loopcounter += 1
-        if min == None:
-            min = checker
-        if nums[checker]<nums[min]:
-            min = checker
-    temp = nums[shifter]
-    nums[shifter] = nums[min]
-    nums[min] = temp
-    min = None
-
-print(loopcounter  , 'loops were required by selection sort')
-
-# Insertion sorting
-nums = originalnums
-
+print(nums)
+print("")
 loopcounterout = 0
 loopcounterin = 0
 for index in range(len(nums)):
@@ -85,4 +38,5 @@ for index in range(len(nums)):
             nums[t] = nums[sample]
             nums[sample] = temp
             sample -= 1
+print(nums)
 print(loopcounterin+loopcounterout , 'loops were required for insertion sort.')
