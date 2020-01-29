c97e46314e7dcb5c0fce0a2c396f80f12a0cc9ce
atharva-bhange <atharva.bhange@gmail.com>
2020-01-29 19:15:19 +0530

    files

--- sorttest.py
+++ sorttest.py
@@ -9,10 +9,7 @@ print("-------------------------------------------------------")
 print("\n")
 
 import random
-def swap(i1 ,i2):
-    nums.insert(i1 , nums[i2])
-    nums.pop(i2+1)
-
+#import timer
 nums = []
 
 while True:
@@ -22,16 +19,22 @@ while True:
     elif num == 'done':
         break
     elif num.startswith('random'):
-
         for t in range(int(num[6:])):
-            nums.append(random.randint(0,10000))
+            nums.append(random.randint(0,10000000000000000))
         break
     else:
         print("Type a number or 'done' to view results.")
-print(nums)
+originalnums = nums
+
+# BUBBLE SORT
+
+def swap(i1 ,i2):
+    nums.insert(i1 , nums[i2])
+    nums.pop(i2+1)
+#print(nums)
 check = 0
-loopcounterin =0
-loopcounterout = 0
+loopcounterout =0
+loopcounterin = 0
 while loopcounterout<len(nums)-1:
     loopcounterout += 1
     for t in range(len(nums)-1):
@@ -42,5 +45,27 @@ while loopcounterout<len(nums)-1:
     if check  ==0  :
         break
     check =0
-print(nums)
-print(loopcounterin + loopcounterout, 'loops were required for sorting.')
+#print(nums)
+print(loopcounterout+loopcounterin, 'loops were required for sorting using bubble sort.')
+
+nums = originalnums
+
+# selection SORT
+
+loopcounter =0
+min = None # storing index not actual value
+
+for shifter in range(0,len(nums)):
+    loopcounter += 1
+    for checker in range(shifter , len(nums)):
+        loopcounter += 1
+        if min == None:
+            min = checker
+        if nums[checker]<nums[min]:
+            min = checker
+    temp = nums[shifter]
+    nums[shifter] = nums[min]
+    nums[min] = temp
+    min = None
+
+print(loopcounter  , 'loops were required by selection sort')
