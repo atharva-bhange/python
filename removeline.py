7b3c6a2ab410cd726021326bef39f6cf3483c3f0
atharva-bhange <atharva.bhange@gmail.com>
2020-03-13 23:26:32 +0530

    .

--- removeline.py
+++ removeline.py
@@ -1,4 +1,15 @@
-file = open(input("name")+".txt", 'r')
+name = input("name")+".txt"
+file = open(name, 'r')
+ls = list(file.read().split("\n"))
+file.close()
+file = open(name, 'w')
+
+for t in ls:
+	file.write(t)
+
+'''
 newfile = open("result.txt", 'w')
+
 for line in file:
 	newfile.write(line[:len(line)-1])
+'''
