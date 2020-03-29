16c4e3ef7aab8352fc570daada693ad84440e32e
atharva-bhange <atharva.bhange@gmail.com>
2020-03-30 01:47:59 +0530

    Square pattern done

--- squarepatern.py
+++ squarepatern.py
@@ -9,72 +9,77 @@ Prob. No. 12
 13  12  11  10   09
 *We are type the number (ie. We type number 5)
 '''
-i =0
-j =0
 
-def refresh():
-    global number
-    global prefix
-    if number > 9 :
-        prefix = ""
+num = int(input("Patern of size:- "))
+size = num ** 0.5
 
-matrix = []
-colm = []
-
-num = int(input("Patern of size"))
-size = int(num ** 0.5)
-check = (num ** 0.5) - size
-
-if check != 0 :
+if size - int(size) != 0  :
     print("Input number should be square number.")
     quit()
-
-for k in range(size):
+size = int(size)
+prefix = '0'
+## Creating Empty matrix
+matrix = []
+for i in range(size):
     matrix.append([])
-for t in range(len(matrix)):
-    for l in range(size):
-        matrix[t].append("")
+    for j in range(size):
+        matrix[i].append("")
+counter = 1
+#
 
-number = 1
-prefix = "0"
-while i == 0 and j < size:
-    matrix[i].remove("")
-    matrix[i].insert(j,prefix+str(number))
-    j+=1
-    number += 1
-j -= 1
-i += 1
-refresh()
-while j == size -1 and i < size:
-    refresh()
-    matrix[i].pop(j)
-    matrix[i].insert(j,prefix+str(number))
-    i+=1
-    number += 1
-i -= 1
-j -=1
-refresh()
-while i == size -1 and j>=0:
-    refresh()
-    matrix[i].pop(j)
-    matrix[i].insert(j,prefix+str(number))
+def check(cnt):
+    global prefix
+    global num
+    if cnt >= num:
+        return
+    if cnt >= 10:
+        prefix = ""
+
+def cycle(i_given , j_given , level):
+    global size
+    global matrix
+    global counter
+    i = i_given
+    j = j_given
+    while  j <= size-1-level:
+        matrix[i][j] = prefix+str(counter)
+        counter+=1
+        check(counter)
+        j+=1
+    j-= 1
+    i +=1
+    while i <= size-1-level:
+        matrix[i][j] = prefix+str(counter)
+        counter +=1
+        check(counter)
+        i+=1
+    i-= 1
     j-=1
-    number += 1
-j += 1
+    while j>=0+level:
+        matrix[i][j] = prefix+str(counter)
+        counter +=1
+        check(counter)
+        j -= 1
+    j+= 1
+    i-= 1
+    while i >=0+level+1:
+        matrix[i][j] = prefix+str(counter)
+        counter += 1
+        check(counter)
+        i -= 1
+    i += 1
 
-refresh()
-while j == 0 and i>0:
-    refresh()
-    matrix[i].pop(j)
-    matrix[i].insert(j,prefix+str(number))
-    i-=1
-    number += 1
+i = 0
+j = 0
+l = 0
+while counter <=num:
+    cycle(i,j,l)
+    i+=1
+    j+=1
+    l+=1
 
-
-print(matrix)
-
-
-#print(matrix)
-print(number)
-print("i is " ,i)
-print("j is " , j)
+## printing matrix
+for i in range(size):
+    for j in range(size):
+        print(matrix[i][j],"" , end = "")
+    print("")
